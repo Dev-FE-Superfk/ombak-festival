57a4955596c747c2451d3a0b04ea59dff8a87f7d
bluesky126 <glenvan12@gmail.com>
2024-06-10 20:06:36 +0700

    video v2

--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -1,7 +1,7 @@
 export default function Videolayer({ src, type }) {
   return (
     <>
-      <video autoplay muted loop playsinline>
+      <video autoPlay muted loop playsinline>
         <source src={src} type={type} />
         Your browser does not support the video tag.
       </video>
