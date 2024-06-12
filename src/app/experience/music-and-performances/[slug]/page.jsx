376a1201cfa66303456cb19bf9f03527f306ce30
Kurniadi17 <kurniadikurniadi1@gmail.com>
2024-06-12 11:40:07 +0700

    fix: minor bugs

--- src/app/experience/music-and-performances/[slug]/page.jsx
+++ src/app/experience/music-and-performances/[slug]/page.jsx
@@ -1,14 +1,13 @@
-"use client";
-import { useRouter, useParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import "../../../../styles/artistdetail.scss";
-import Link from "next/link";
-import Image from "next/image";
-import { ArtistCard } from "@/components";
+'use client';
+import {useParams} from 'next/navigation';
+import {useEffect, useState} from 'react';
+import '../../../../styles/artistdetail.scss';
+import Link from 'next/link';
+import Image from 'next/image';
+import {ArtistCard} from '@/components';
 
 export default function ArtistDetail() {
-  const router = useRouter();
-  const { slug } = useParams(); // Mengakses slug dari router.query
+  const {slug} = useParams();
 
   const [artistdetail, setArtistdetail] = useState(null);
   const [artists, setArtists] = useState(null);
@@ -18,15 +17,15 @@ export default function ArtistDetail() {
       const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/music-and-performances/detail?slug=${slug}`;
 
       fetch(apiUrl, {
-        method: "GET",
+        method: 'GET',
         headers: {
-          "x-api-key": "bda07dc4-cab9-4148-ac9c-7a44c3c55wqr9",
-          "Content-Type": "application/json",
+          'x-api-key': 'bda07dc4-cab9-4148-ac9c-7a44c3c55wqr9',
+          'Content-Type': 'application/json',
         },
       })
         .then((response) => {
           if (!response.ok) {
-            throw new Error("Network response was not ok");
+            throw new Error('Network response was not ok');
           }
           return response.json();
         })
@@ -38,7 +37,7 @@ export default function ArtistDetail() {
           setArtists(artists);
         })
         .catch((error) => {
-          console.error("Error fetching data: ", error);
+          console.error('Error fetching data: ', error);
         });
     }
   }, [slug]);
@@ -48,69 +47,75 @@ export default function ArtistDetail() {
   }
 
   return (
-    <div className="section_artist_detail">
-      <div className="container">
-        <div className="back_button">
-          <Link href="/experience/music-and-performances">Back</Link>
+    <div className='section_artist_detail'>
+      <div className='container'>
+        <div className='back_button'>
+          <Link href='/experience/music-and-performances'>Back</Link>
         </div>
-        <div className="artist_detail">
-          <div className="detail_left">
-            <h3 className="mobile">{artistdetail.name}</h3>
+        <div className='artist_detail'>
+          <div className='detail_left'>
+            <h3 className='mobile'>{artistdetail.name}</h3>
             <Image
               src={artistdetail.image}
               alt={artistdetail.name}
               width={500}
-              height={500}></Image>
+              height={500}
+            ></Image>
           </div>
-          <div className="detail_right">
-            <div className="dr_info">
-              <h3 className="desktop">{artistdetail.name}</h3>
+          <div className='detail_right'>
+            <div className='dr_info'>
+              <h3 className='desktop'>{artistdetail.name}</h3>
               <div
-                className="artist_desc"
+                className='artist_desc'
                 dangerouslySetInnerHTML={{
                   __html: artistdetail.description,
-                }}></div>
-              <div className="sosial_media">
+                }}
+              ></div>
+              <div className='sosial_media'>
                 {artistdetail.instagram && (
-                  <div className="sm_box">
+                  <div className='sm_box'>
                     <Link
-                      className="sm_instagram"
+                      className='sm_instagram'
                       href={artistdetail.instagram}
-                      target="_blank"
-                      rel="noopener noreferrer">
+                      target='_blank'
+                      rel='noopener noreferrer'
+                    >
                       Instagram
                     </Link>
                   </div>
                 )}
                 {artistdetail.tiktok && (
-                  <div className="sm_box">
+                  <div className='sm_box'>
                     <Link
-                      className="sm_tiktok"
+                      className='sm_tiktok'
                       href={artistdetail.tiktok}
-                      target="_blank"
-                      rel="noopener noreferrer">
+                      target='_blank'
+                      rel='noopener noreferrer'
+                    >
                       Tiktok
                     </Link>
                   </div>
                 )}
                 {artistdetail.facebook && (
-                  <div className="sm_box">
+                  <div className='sm_box'>
                     <Link
-                      className="sm_facebook"
+                      className='sm_facebook'
                       href={artistdetail.facebook}
-                      target="_blank"
-                      rel="noopener noreferrer">
+                      target='_blank'
+                      rel='noopener noreferrer'
+                    >
                       Facebook
                     </Link>
                   </div>
                 )}
                 {artistdetail.youtube && (
-                  <div className="sm_box">
+                  <div className='sm_box'>
                     <Link
-                      className="sm_youtube"
+                      className='sm_youtube'
                       href={artistdetail.youtube}
-                      target="_blank"
-                      rel="noopener noreferrer">
+                      target='_blank'
+                      rel='noopener noreferrer'
+                    >
                       Youtube
                     </Link>
                   </div>
@@ -119,14 +124,15 @@ export default function ArtistDetail() {
             </div>
           </div>
         </div>
-        <div className="discover_artist">
+        <div className='discover_artist'>
           <h2>Discover More Artistes</h2>
-          <div className="content">
+          <div className='content'>
             {artists.map((artist, index) => (
               <ArtistCard
                 key={artist.id}
                 artist={artist}
-                index={index}></ArtistCard>
+                index={index}
+              ></ArtistCard>
             ))}
           </div>
         </div>
