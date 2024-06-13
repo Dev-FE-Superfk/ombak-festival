48dbbb0dc0d25b8cebc6b8a04f5c1daada08fb7d
mariofredo <54829954+mariofredo@users.noreply.github.com>
2024-06-13 10:05:30 +0700

    add And more to come

--- src/app/experience/music-and-performances/page.jsx
+++ src/app/experience/music-and-performances/page.jsx
@@ -1,31 +1,31 @@
-"use client";
-import Image from "next/image";
-import { useEffect, useState } from "react";
-import Link from "next/link";
-import "../../../styles/experience.scss";
-import { ArtistCard } from "@/components";
+'use client';
+import Image from 'next/image';
+import {useEffect, useState} from 'react';
+import Link from 'next/link';
+import '../../../styles/experience.scss';
+import {ArtistCard} from '@/components';
 
 export default function Experience() {
   const [musicperformance, setMusicperformance] = useState({
-    image: "",
-    title: "",
-    description: "",
+    image: '',
+    title: '',
+    description: '',
   });
   const [artists, setArtists] = useState([]);
   useEffect(() => {
     // Gantilah URL ini dengan URL API Anda
-    const apiUrl = process.env.NEXT_PUBLIC_API_URL + "/music-and-performances";
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL + '/music-and-performances';
 
     fetch(apiUrl, {
-      method: "GET",
+      method: 'GET',
       headers: {
-        "x-api-key": "bda07dc4-cab9-4148-ac9c-7a44c3c55wqr9",
-        "Content-Type": "application/json",
+        'x-api-key': 'bda07dc4-cab9-4148-ac9c-7a44c3c55wqr9',
+        'Content-Type': 'application/json',
       },
     })
       .then((response) => {
         if (!response.ok) {
-          throw new Error("Network response was not ok");
+          throw new Error('Network response was not ok');
         }
         return response.json();
       })
@@ -41,58 +41,62 @@ export default function Experience() {
         setArtists(artists);
       })
       .catch((error) => {
-        console.error("Error fetching data: ", error);
+        console.error('Error fetching data: ', error);
       });
   }, []);
 
   return (
-    <div className="section_music_performance">
-      <div className="music_performance_banner">
-        <div className="mp_image">
+    <div className='section_music_performance'>
+      <div className='music_performance_banner'>
+        <div className='mp_image'>
           <Image
             src={musicperformance.image}
             alt={musicperformance.title}
             width={600}
-            height={100}></Image>
+            height={100}
+          ></Image>
         </div>
-        <div className="mp_info">
-          <div className="mpi_box">
-            <span className="sub_title">Experience</span>
-            <h3 className="title">{musicperformance.title}</h3>
+        <div className='mp_info'>
+          <div className='mpi_box'>
+            <span className='sub_title'>Experience</span>
+            <h3 className='title'>{musicperformance.title}</h3>
             <p>{musicperformance.description}</p>
           </div>
         </div>
       </div>
-      <div className="artist_list">
-        <div className="container">
+      <div className='artist_list'>
+        <div className='container'>
           {artists.map((artist, index) => (
             <ArtistCard
               key={artist.id}
               artist={artist}
-              index={index}></ArtistCard>
+              index={index}
+            ></ArtistCard>
           ))}
+          <div className='artist_amtc'>And more to come</div>
         </div>
       </div>
-      <div className="more_experience">
-        <div className="container">
+      <div className='more_experience'>
+        <div className='container'>
           <h3>More experiences</h3>
-          <div className="row_flex">
-            <Link className="me_box fnb" href="/experience/food-and-beverages">
+          <div className='row_flex'>
+            <Link className='me_box fnb' href='/experience/food-and-beverages'>
               <h4>F&amp;B</h4>
-              <div className="button">Explore</div>
+              <div className='button'>Explore</div>
             </Link>
-            <Link className="me_box ombakkids" href="/experience/ombak-kids">
+            <Link className='me_box ombakkids' href='/experience/ombak-kids'>
               <h4>Ombak Kids</h4>
-              <div className="button">Explore</div>
+              <div className='button'>Explore</div>
             </Link>
             <Link
-              className="me_box visualarts"
-              href="/experience/visual-arts-and-craft">
+              className='me_box visualarts'
+              href='/experience/visual-arts-and-craft'
+            >
               <h4>
                 Visual Arts <br />
                 &amp; Craft
               </h4>
-              <div className="button">Explore</div>
+              <div className='button'>Explore</div>
             </Link>
           </div>
         </div>
