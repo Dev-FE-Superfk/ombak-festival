2db388864b59acb8a359b9095f8ae6f3f28257c3
Dev-FE-Superfk <dev-fe@superfk.co>
2024-06-21 11:03:57 +0700

    meta, og, twitter

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -28,13 +28,23 @@ export default function RootLayout({children}) {
       />
       <meta
         name='keywords'
-        content='Family friendly activities, Music festival, Music festival in Johor, Arts and Crafts, Beach festival, Weekend getaway ideas, Events for family, Malaysia festivals, Upcoming concerts 2024, Resort activities'
+        content='Ombak festival, Family friendly activities, Music festival, Music festival in Johor, Arts and Crafts, Beach festival, Weekend getaway ideas, Events for family, Malaysia festivals, Upcoming concerts 2024, Resort activities'
       />
       <meta
         name='viewport'
         content='width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'
       />
-      <link rel='icon' href='/favicon.ico' />
+      <meta property="og:locale" content="en_US" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Ombak Festival At Desaru Coast Johor"/>
+      <meta property="og:description" content="A luxurious weekend getaway filled with music, arts, culture and family fun! Get your ticket with our partnering hotels now"/>
+      <meta property="og:url" content="https://www.ombakfestival.com/" />
+      <meta property="og:site_name" content="Ombak Festival At Desaru Coast Johor" />
+      <meta property="og:image" content="https://www.ombakfestival.com/ombak_meta.png" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:description" content="A luxurious weekend getaway filled with music, arts, culture and family fun! Get your ticket with our partnering hotels now"/>
+      <meta name="twitter:title" content="Ombak Festival At Desaru Coast Johor" />
+      <meta name="twitter:image" content="https://www.ombakfestival.com/ombak_meta.png" />
       <script
             dangerouslySetInnerHTML={{
               __html: `
