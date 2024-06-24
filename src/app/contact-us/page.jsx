afa789a9ee16679308d8dfc0feb7ce84f6d9ca69
Dev-FE-Superfk <dev-fe@superfk.co>
2024-06-24 16:37:38 +0700

    phase 1.2 done

---
+++ src/app/contact-us/page.jsx
@@ -0,0 +1,164 @@
+'use client';
+import Link from 'next/link';
+import { useState } from 'react';
+import '../../styles/contact.scss';
+import { sanitize } from 'dompurify';
+
+export default function ContactUs() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        body: ''
+    });
+    const [errors, setErrors] = useState({});
+    const [isSending, setIsSending] = useState(false);
+    const [sendSuccess, setSendSuccess] = useState(false);
+    
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        // Cleanse input to prevent script injection using DomPurify
+        const sanitizedValue = sanitize(value);
+        setFormData({
+            ...formData,
+            [name]: sanitizedValue
+        });
+
+        // Clear error message for textarea when user starts typing
+        if (name === 'body') {
+            setErrors({
+                ...errors,
+                body: ''
+            });
+        }
+    };
+
+    const validateForm = () => {
+        const newErrors = {};
+        if (!formData.name) newErrors.name = 'Please enter your name';
+        if (!formData.email) newErrors.email = 'Please enter a valid email address';
+        if (!formData.body) newErrors.body = 'This field cannot be empty';
+        if (/\b(script|<|>)/i.test(formData.name)) newErrors.name = 'Invalid characters entered';
+        if (/\b(script|<|>)/i.test(formData.email)) newErrors.email = 'Invalid characters entered';
+        if (/\b(script|<|>)/i.test(formData.body)) newErrors.body = 'Invalid characters entered';
+        return newErrors;
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const validationErrors = validateForm();
+        if (Object.keys(validationErrors).length > 0) {
+            setErrors(validationErrors);
+            return;
+        }
+
+        setIsSending(true);
+
+        try {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/send-mail`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-api-key': 'bda07dc4-cab9-4148-ac9c-7a44c3c55wqr9'
+                },
+                body: JSON.stringify({
+                    name: sanitize(formData.name),
+                    email: sanitize(formData.email),
+                    body: sanitize(formData.body)
+                })
+            });
+
+            if (response.ok) {
+                setSendSuccess(true);
+                setFormData({ name: '', email: '', body: '' });
+                setErrors({});
+            } else {
+                const errorData = await response.json();
+                console.error('Error sending email:', errorData);
+                alert('Failed to send email.');
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            alert('Failed to send email.');
+        } finally {
+            setIsSending(false);
+            setTimeout(() => {
+                setSendSuccess(false);
+            }, 3000);
+        }
+    };
+
+    return (
+        <>
+            <div className="section_contact">
+                <div className="container">
+                    <h2>Contact Us</h2>
+                    <div className="row_flex">
+                        <div className="contact_left">
+                            <h3>General Enquiries</h3>
+                            <p>Please visit our <Link href='/info?tag=faq'>FAQs</Link> page before sending us a body. And if you still don't find what you are looking for, feel free to send us a body!</p>
+                            <p>We will make every effort to respond within 5 business days, however, response times can be affected due to the high volume of emails we receive each day.</p>
+                        </div>
+                        <div className="contact_right">
+                            <form onSubmit={handleSubmit}>
+                                <div className="form_box">
+                                    <span>Your Name</span>
+                                    <input
+                                        type="text"
+                                        name="name"
+                                        placeholder='eg. John Doe'
+                                        value={formData.name}
+                                        onChange={handleChange}
+                                        className={errors.name ? 'error' : ''}
+                                    />
+                                    {errors.name && <p className="error">{errors.name}</p>}
+                                </div>
+                                <div className="form_box">
+                                    <span>Email Address</span>
+                                    <input
+                                        type="email"
+                                        name="email"
+                                        placeholder='abc@example.com'
+                                        value={formData.email}
+                                        onChange={handleChange}
+                                        className={errors.email ? 'error' : ''}
+                                    />
+                                    {errors.email && <p className="error">{errors.email}</p>}
+                                </div>
+                                <div className="form_box">
+                                    <span>What can we help you?</span>
+                                    <textarea
+                                        name="body"
+                                        placeholder='Write here...'
+                                        value={formData.body}
+                                        onChange={handleChange}
+                                        className={errors.body ? 'error' : ''}
+                                    ></textarea>
+                                    {errors.body && <p className="error">{errors.body}</p>}
+                                </div>
+                                <div className="form_box">
+                                    <button type="submit" disabled={isSending}>
+                                        {isSending ? 'Send' : 'Send'}
+                                    </button>
+                                    {sendSuccess && <p className="success">Email sent successfully!</p>}
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="section_bottom">
+                <div className="container">
+                    <div className="sb_box">
+                        <h3>Sponsorship</h3>
+                        <p>For sponsorship enquiries, please email at <br/><Link href="mailto:example@ombakfestival.com">example@ombakfestival.com</Link></p>
+                    </div>
+                    <div className="sb_box">
+                        <h3>Press</h3>
+                        <p>For press enquiries, please email at <br/><Link href="mailto:example@ombakfestival.com">example@ombakfestival.com</Link></p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
--- src/app/info/page.jsx
+++ src/app/info/page.jsx
@@ -1,7 +1,8 @@
 'use client';
-import {Suspense} from 'react';
-import {useSearchParams} from 'next/navigation';
-import {useEffect, useState} from 'react';
+import { Suspense } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
 import Link from 'next/link';
 import '../../styles/info.scss';
 
@@ -12,6 +13,7 @@ function Info() {
   const [details, setDetails] = useState(null);
   const [error, setError] = useState(null);
   const [visibleItems, setVisibleItems] = useState({});
+  const classes = ['wave1', 'wave2', 'wave3', 'wave4'];
 
   useEffect(() => {
     setVisibleItems({});
@@ -32,9 +34,15 @@ function Info() {
           return response.json();
         })
         .then((data) => {
-          setDetails(data.data.detail);
-
-          toggleVisibility(data.data.detail[0].id, true);
+          if (tag === 'general_info') {
+            setDetails(data.data.detail);
+            toggleVisibility(data.data.detail.id, true);
+          } else {
+            setDetails(data.data.detail || []);
+            if (data.data.detail && data.data.detail.length > 0) {
+              toggleVisibility(data.data.detail[0].id, true);
+            }
+          }
         })
         .catch((error) => {
           console.error('Error fetching data: ', error);
@@ -53,6 +61,7 @@ function Info() {
   useEffect(() => {
     console.log(visibleItems, 'test');
   }, [visibleItems]);
+
   if (error) {
     return <div>Error: {error}</div>;
   }
@@ -70,6 +79,11 @@ function Info() {
       </div>
       <div className='si_nav'>
         <div className='container'>
+          <div className={`si_nav_box ${tag === 'general_info' ? 'active' : ''}`}>
+            <Link scroll={false} href='/info?tag=general_info'>
+              General Info
+            </Link>
+          </div>
           <div className={`si_nav_box ${tag === 'info' ? 'active' : ''}`}>
             <Link scroll={false} href='/info?tag=info'>
               Getting Here
@@ -85,7 +99,11 @@ function Info() {
       <div className='info_middle'>
         <div className='container'>
           <div className='title_box'>
-            {tag === 'info' ? (
+            {tag === 'general_info' ? (
+              <>
+                <h3 className='no_border'>General Info</h3>
+              </>
+            ) : tag === 'info' ? (
               <>
                 <h3>Getting Here</h3>
               </>
@@ -95,60 +113,104 @@ function Info() {
               </>
             ) : null}
           </div>
-          {details.map((detail, index) => (
-            <div className='info_box'>
-              <div
-                className='info_title'
-                onClick={() =>
-                  toggleVisibility(detail.id, !visibleItems[detail.id])
-                }
-              >
-                <h3>{detail.name}</h3>
-                <div
-                  className={`toggle_btn ${
-                    visibleItems[detail.id] ? 'active' : ''
-                  }`}
-                ></div>
-              </div>
-              {visibleItems[detail.id] && (
-                <div>
-                  {detail.detail ? (
-                    detail.detail.map((item) => (
-                      <div key={item.id} className='info_content'>
-                        <div className='info_question'>
-                          <h4>{item.question}</h4>
+          {tag === 'general_info' && details ? (
+            <div className='general_info_content'>
+              {details.content && Object.keys(details.content).map((sectionId) => {
+                const section = details.content[sectionId];
+                return (
+                  <div key={sectionId} className='info_section'>
+                    {section.contents && Object.keys(section.contents).map((contentId) => {
+                      const content = section.contents[contentId];
+                      return (
+                        <div key={contentId} className='content_item'>
+                          {content.items && content.items.map((item, index) => (
+                            <div key={index} className={`item ${
+                        classes[index % classes.length]
+                      }`}>
+                              <div className='item_image'>
+                                <Image src={item.v_page_element_image} alt={item.v_page_element_image_title} width={200} height={200}></Image>
+                              </div>
+                              <div className="item_info">
+                                <h4>{item.v_page_element_image_title}</h4>
+                                <div
+                                  dangerouslySetInnerHTML={{
+                                    __html: item.v_page_element_text,
+                                  }}
+                                ></div>
+                              </div>
+                            </div>
+                          ))}
                         </div>
+                      );
+                    })}
+                    <div className='faq_info'>
+                      <h4>Have questions and want to know more?</h4>
+                      <button>
+                        <Link href='/info?tag=faq'>Check Out FAQ</Link>
+                      </button>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          ) : Array.isArray(details) ? (
+            details.map((detail) => (
+              <div className='info_box' key={detail.id}>
+                <div
+                  className='info_title'
+                  onClick={() =>
+                    toggleVisibility(detail.id, !visibleItems[detail.id])
+                  }
+                >
+                  <h3>{detail.name}</h3>
+                  <div
+                    className={`toggle_btn ${
+                      visibleItems[detail.id] ? 'active' : ''
+                    }`}
+                  ></div>
+                </div>
+                {visibleItems[detail.id] && (
+                  <div>
+                    {detail.detail ? (
+                      detail.detail.map((item) => (
+                        <div key={item.id} className='info_content'>
+                          <div className='info_question'>
+                            <h4>{item.question}</h4>
+                          </div>
+                          <div className='info_answer'>
+                            <div
+                              dangerouslySetInnerHTML={{
+                                __html: item.answer,
+                              }}
+                            ></div>
+                          </div>
+                        </div>
+                      ))
+                    ) : (
+                      <div className='info_content'>
                         <div className='info_answer'>
                           <div
                             dangerouslySetInnerHTML={{
-                              __html: item.answer,
+                              __html: detail.description,
                             }}
                           ></div>
                         </div>
                       </div>
-                    ))
-                  ) : (
-                    <div className='info_content'>
-                      <div className='info_answer'>
-                        <div
-                          dangerouslySetInnerHTML={{
-                            __html: detail.description,
-                          }}
-                        ></div>
-                      </div>
-                    </div>
-                  )}
-                </div>
-              )}
-            </div>
-          ))}
+                    )}
+                  </div>
+                )}
+              </div>
+            ))
+          ) : (
+            <div>Invalid details format</div>
+          )}
         </div>
       </div>
     </div>
   );
 }
 
-export default function infoWrapper() {
+export default function InfoWrapper() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Info />
