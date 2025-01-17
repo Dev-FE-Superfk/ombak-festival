e5cddb1aa8b11ad72cf95ff52ab6e0a4334609ac
Dev-FE-Superfk <dev-fe@superfk.co>
2025-01-17 15:36:40 +0700

    test again

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -46,8 +46,8 @@ export default function RootLayout({children}) {
               `,
             }}
           />
-      <script async src="https://www.googletagmanager.com/gtag/js?id=GTM-WXVWJTLM"></script>
-      <script
+      {/* <script async src="https://www.googletagmanager.com/gtag/js?id=GTM-WXVWJTLM"></script> */}
+      {/* <script
         dangerouslySetInnerHTML={{
               __html: `
         window.dataLayer = window.dataLayer || [];
@@ -56,7 +56,20 @@ export default function RootLayout({children}) {
         gtag('config', 'GTM-WXVWJTLM');
         `,
         }}
-      />
+      /> */}
+      <script
+          id="gtm-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WXVWJTLM');
+            `,
+          }}
+        />
       <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -74,6 +87,8 @@ export default function RootLayout({children}) {
           }}
         />
       <body>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WXVWJTLM"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <Header />
         <section id='root'>{children}</section>
         <Footer />
